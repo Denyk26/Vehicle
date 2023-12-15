704325d0416df200f57927019747e9e352309a68
Denyk26 <deniss.maks@mail.ru>
2023-12-15 16:22:39 +0100

    Prepared class Vehicle

---
+++ src/Vehicle.java
@@ -0,0 +1,13 @@
+public class Vehicle {
+
+    public String brand;
+    public int year;
+
+    public Vehicle(String brand,int year){
+        this.brand = brand;
+        this.year = year;
+    }
+}
+
+//Определите класс Vehicle как родительский класс. Добавьте поля, такие как brand (марка) и year (год выпуска).
+// Напишите конструктор для инициализации этих полей.
