9c8e1ff99f23b1de5a49a4f15db92bb6783d12a7
Denyk26 <deniss.maks@mail.ru>
2023-12-15 16:28:31 +0100

    Made class Car

---
+++ src/Car.java
@@ -0,0 +1,12 @@
+public class Car extends Vehicle{
+
+    public String model;
+
+    public Car(String brand,int year,String model){
+        super(brand,year);
+        this.model = model;
+    }
+}
+
+//Создайте класс Car, который будет являться дочерним для класса Vehicle.
+// Добавьте дополнительное поле model (модель) и напишите конструктор для инициализации всех полей.
