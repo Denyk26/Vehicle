b69bc21b2006ed8f30454be9cbb5fb45dffa0663
Denyk26 <deniss.maks@mail.ru>
2023-12-15 17:07:37 +0100

    Made method displayInnfo and deduced in class Main

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
 
         Car car = new Car("BMW", 2020, "M3");
         car.info();
+        Vehicle vehicle = new Vehicle("Audi",2012);
+        vehicle.displayInfo();
     }
 }
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -7,7 +7,12 @@ public class Vehicle {
         this.brand = brand;
         this.year = year;
     }
+    public void displayInfo(){
+        System.out.println("Марка: " + brand + " , Год выпуска: " + year);
+    }
 }
 
 //Определите класс Vehicle как родительский класс. Добавьте поля, такие как brand (марка) и year (год выпуска).
 // Напишите конструктор для инициализации этих полей.
+
+//Добавьте в класс Vehicle метод displayInfo(), который будет выводить на консоль информацию о марке и годе выпуска.
