283be863ee92295438afdbd2ae26d607c6edb2d3
Denyk26 <deniss.maks@mail.ru>
2023-12-15 16:34:51 +0100

    Prepare class Main

--- src/Car.java
+++ src/Car.java
@@ -1,12 +1,14 @@
-public class Car extends Vehicle{
+public class Car extends Vehicle {
 
     public String model;
 
-    public Car(String brand,int year,String model){
-        super(brand,year);
+    public Car(String brand, int year, String model) {
+        super(brand, year);
         this.model = model;
     }
 }
 
 //Создайте класс Car, который будет являться дочерним для класса Vehicle.
 // Добавьте дополнительное поле model (модель) и напишите конструктор для инициализации всех полей.
+
+//Перепишите метод toString() в классе Car, чтобы он включал информацию о марке, модели и годе выпуска автомобиля.
